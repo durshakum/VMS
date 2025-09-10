f292f8dbe5a1b8c70e5d09c538f6330f2adff1fe
Shankar Kumar <durshakum@gmail.com>
2025-09-10 12:14:46 +0530

    Added a Line

--- src/test/java/test/BrokenLinks.java
+++ src/test/java/test/BrokenLinks.java
@@ -77,6 +77,7 @@ public void name() throws InterruptedException {
 	 Thread.sleep(3);
 	 driver.switchTo().newWindow(WindowType.TAB);
 	 Thread.sleep(10);
+	 Thread.sleep(10);
 	 Set<String> handleSet =driver.getWindowHandles();
 	 System.out.println("Size of window is "+handleSet.size());
 	 Iterator iterator = handleSet.iterator();
